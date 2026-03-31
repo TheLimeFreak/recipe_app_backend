c4ec9bd66d03693428f322c261a8870cdf28a45d
Thomas Birk <thomas_birk98@hotmail.com>
2026-03-31 17:52:20 +0200

    Added the user model and basic security framework

---
+++ src/main/java/no/thomasbirk/recipe/models/User.java
@@ -0,0 +1,22 @@
+package no.thomasbirk.recipe.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+    private String username;
+}
